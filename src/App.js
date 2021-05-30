7a53671e5663f0d8da49ee192716ecf073166a50
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-05-31 00:57:12 +0530

    feat: add particles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import Particles from "react-particles-js";
 import "./App.css";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
+import Rank from "./components/Rank/Rank";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    size: {
+      value: 3,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* 
       <FaceRecoganition /> */}
